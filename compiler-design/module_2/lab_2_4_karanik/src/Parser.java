0b014e6c41ea0ad3105f79b0949df476edf35c6f
AndreyKaranik <qws.main@gmail.com>
2024-06-04 23:59:29 +0300

    update

--- compiler-design/module_2/lab_2_4_karanik/src/Parser.java
+++ compiler-design/module_2/lab_2_4_karanik/src/Parser.java
@@ -27,11 +27,22 @@ public class Parser {
         set1.add(DomainTag.KW_BOOL);
         set1.add(DomainTag.KW_VOID);
         first.put("NFunctionDeclaration", set1);
+
         HashSet<DomainTag> set2 = new HashSet<>();
         set2.add(DomainTag.KW_INT);
         set2.add(DomainTag.KW_CHAR);
         set2.add(DomainTag.KW_BOOL);
         first.put("NType", set2);
+
+        HashSet<DomainTag> set3 = new HashSet<>();
+        set3.add(DomainTag.IDENTIFIER);
+        set3.add(DomainTag.LEFT_PAR);
+        set3.add(DomainTag.DECIMAL_INTEGER_CONSTANT);
+        set3.add(DomainTag.NON_DECIMAL_INTEGER_CONSTANT);
+        set3.add(DomainTag.SYMBOLIC_CONSTANT);
+        set3.add(DomainTag.BOOLEAN_CONSTANT);
+        set3.add(DomainTag.KW_NULL);
+        first.put("NBottomExpr", set3);
     }
 
     public void reportError() {
@@ -175,5 +186,160 @@ public class Parser {
         }
 
         return new AbstractTree.Type(prim, n);
+    }
+
+    // NExpr ::= NAndExpr ('|' NAndExpr | '@' NAndExpr)*
+    public void NExpr() {
+        NAndExpr();
+        while (sym.getTag().equals(DomainTag.OR_XOR_OP)) {
+            sym = scanner.nextToken();
+            NAndExpr();
+        }
+    }
+
+    // NAndExpr ::= NCmpExpr ('&' NCmpExpr)*
+    public void NAndExpr() {
+        NCmpExpr();
+        while (sym.getTag().equals(DomainTag.AND_OP)) {
+            sym = scanner.nextToken();
+            NCmpExpr();
+        }
+    }
+
+    // NCmpExpr ::= NFuncCallExpr (NCmpOp NFuncCallExpr)*
+    public void NCmpExpr() {
+        NFuncCallExpr();
+        while (first.get("NCmpOp").contains(sym.getTag())) {
+            NCmpOp();
+            NFuncCallExpr();
+        }
+    }
+
+    // NCmpOp ::= EQ_OP | ORD_OP
+    public void NCmpOp() {
+        if (sym.getTag().equals(DomainTag.EQ_OP)) {
+            sym = scanner.nextToken();
+        } else if (sym.getTag().equals(DomainTag.ORD_OP)) {
+            sym = scanner.nextToken();
+        } else {
+            reportError();
+        }
+    }
+
+    // NFuncCallExpr ::= NArithmExpr | (IDENTIFIER '<-' NArgs)
+    public void NFuncCallExpr() {
+        if (sym.getTag().equals(DomainTag.IDENTIFIER)) {
+            sym = scanner.nextToken();
+            if (sym.getTag().equals(DomainTag.LEFT_ARROW)) {
+                sym = scanner.nextToken();
+                NArgs();
+            } else {
+                reportError();
+            }
+        } else {
+            NArithmExpr();
+        }
+    }
+
+    // NArgs ::= NArithmExpr (',' NArithmExpr)*
+    public void NArgs() {
+        NArithmExpr();
+        while (sym.getTag().equals(DomainTag.COMMA)) {
+            sym = scanner.nextToken();
+            NArithmExpr();
+        }
+    }
+
+    // NArithmExpr ::= NTerm (('+' | '-') NTerm)*
+    public void NArithmExpr() {
+        NTerm();
+        while (sym.getTag().equals(DomainTag.PLUS_MINUS_OP)) {
+            sym = scanner.nextToken();
+            NTerm();
+        }
+    }
+
+    // NTerm ::= NFactor (('*' | '/' | '%') NFactor)*
+    public void NTerm() {
+        NFactor();
+        while (sym.getTag().equals(DomainTag.MUL_DIV_REM_OP)) {
+            sym = scanner.nextToken();
+            NFactor();
+        }
+    }
+
+    // NFactor ::= NPower ('^' NFactor)?
+    public void NFactor() {
+        NPower();
+        if (sym.getTag().equals(DomainTag.POWER_OP)) {
+            sym = scanner.nextToken();
+            NFactor();
+        }
+    }
+
+    // NPower ::= NArrExpr | (('!' | '-') NPower) | (NType NBottomExpr)
+    public void NPower() {
+        if (sym.getTag().equals(DomainTag.NOT_MINUS_OP)) {
+            sym = scanner.nextToken();
+        } else if (first.get("NType").contains(sym.getTag())) {
+            NType();
+            NBottomExpr();
+        } else {
+            NArrExpr();
+        }
+    }
+
+    // NArrExpr ::= NBottomExpr | (NArrExpr NBottomExpr) | NStringConstant
+    public void NArrExpr() {
+        if (sym.getTag().equals(DomainTag.STRING_SECTION)) {
+            NStringConstant();
+        } else if (first.get("NBottomExpr").contains(sym.getTag())) {
+            NBottomExpr();
+        } else {
+            NArrExpr();
+            NBottomExpr();
+        }
+    }
+
+    // NBottomExpr ::= IDENTIFIER | NConst | ('(' NExpr ')')
+    public void NBottomExpr() {
+        if (sym.getTag().equals(DomainTag.IDENTIFIER)) {
+            sym = scanner.nextToken();
+        } else if (sym.getTag().equals(DomainTag.LEFT_PAR)) {
+            sym = scanner.nextToken();
+            NExpr();
+            if (sym.getTag().equals(DomainTag.RIGHT_PAR)) {
+                sym = scanner.nextToken();
+            } else {
+                reportError();
+            }
+        } else {
+            NConst();
+        }
+    }
+
+    // NStringConstant ::= STRING_SECTION STRING_SECTION*
+    public void NStringConstant() {
+        if (sym.getTag().equals(DomainTag.STRING_SECTION)) {
+            sym = scanner.nextToken();
+        } else {
+            reportError();
+        }
+        while (sym.getTag().equals(DomainTag.STRING_SECTION)) {
+            sym = scanner.nextToken();
+        }
+    }
+
+    // NConst ::= DECIMAL_INTEGER_CONSTANT | NON_DECIMAL_INTEGER_CONSTANT | SYMBOLIC_CONSTANT | BOOLEAN_CONSTANT | KW_NULL
+    public void NConst() {
+        if (sym.getTag().equals(DomainTag.DECIMAL_INTEGER_CONSTANT)
+                | sym.getTag().equals(DomainTag.NON_DECIMAL_INTEGER_CONSTANT)
+                | sym.getTag().equals(DomainTag.SYMBOLIC_CONSTANT)
+                | sym.getTag().equals(DomainTag.BOOLEAN_CONSTANT)
+                | sym.getTag().equals(DomainTag.KW_NULL)) {
+            sym = scanner.nextToken();
+        } else {
+            reportError();
+        }
     }
 }
