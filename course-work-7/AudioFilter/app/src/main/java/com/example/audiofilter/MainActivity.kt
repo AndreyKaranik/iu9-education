3e43fcfe2a8847a5118ba4ba039b31e6f9ffce96
Andrey Karanik <qws.main@gmail.com>
2025-02-09 01:25:32 +0300

    Update

--- course-work-7/AudioFilter/app/src/main/java/com/example/audiofilter/MainActivity.kt
+++ course-work-7/AudioFilter/app/src/main/java/com/example/audiofilter/MainActivity.kt
@@ -13,15 +13,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -46,6 +51,7 @@ fun AudioProcessorScreen(viewModel: MainActivityViewModel) {
     val context = LocalContext.current
     val selectedFileName by viewModel.selectedFileName.collectAsState()
     val processedFileUri by viewModel.processedFileUri.collectAsState()
+    val scrollState = rememberScrollState()
 
     val filePickerLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent(),
@@ -62,7 +68,6 @@ fun AudioProcessorScreen(viewModel: MainActivityViewModel) {
 
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             item {
                 Button(onClick = { filePickerLauncher.launch("audio/*") }) {
@@ -94,10 +99,12 @@ fun AudioProcessorScreen(viewModel: MainActivityViewModel) {
             }
 
             item {
+                Spacer(modifier = Modifier.size(16.dp))
                 processedAudio?.let {
-                    Text("Аудио график")
                     AudioWaveformView(
-                        audioData = it
+                        audioData = it,
+                        sampleRate = viewModel.sampleRate,
+                        scrollState = scrollState
                     )
                 }
             }
@@ -106,28 +113,70 @@ fun AudioProcessorScreen(viewModel: MainActivityViewModel) {
 }
 
 @Composable
-fun AudioWaveformView(audioData: ShortArray) {
-    Canvas(modifier = Modifier
-        .fillMaxWidth()
-        .height(200.dp)
-    ) {
-        val width = size.width
-        val height = size.height
-        val centerY = height / 2
-        val amplitude = height / 4
+fun AudioWaveformView(audioData: ShortArray, sampleRate: Int, scrollState: ScrollState) {
 
-        val step = audioData.size / width.toInt()
-        for (i in 0 until width.toInt()) {
-            val sampleIndex = i * step
-            if (sampleIndex < audioData.size) {
-                val sample = audioData[sampleIndex]
-                val normalizedSample = sample.toFloat() / Short.MAX_VALUE.toFloat() * amplitude
-                drawLine(
-                    color = Color.Blue,
-                    start = androidx.compose.ui.geometry.Offset(i.toFloat(), centerY - normalizedSample),
-                    end = androidx.compose.ui.geometry.Offset(i.toFloat(), centerY + normalizedSample),
-                    strokeWidth = 1f
-                )
+    var scale by remember { mutableStateOf(1f) }
+
+
+
+    Text(
+        text = "Масштаб отображения:",
+        fontSize = 16.sp,
+    )
+    Slider(
+        value = scale,
+        onValueChange = { newScale ->
+            scale = newScale
+        },
+        valueRange = 0.2f..2.0f,
+        steps = 20,
+        modifier = Modifier.padding(bottom = 16.dp)
+    )
+
+    Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+        Text("Мин: 0.2")
+        Text("Текущее: ${"%.2f".format(scale)}")
+        Text("Макс: 2.0")
+    }
+
+    val v = audioData.size / sampleRate
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .horizontalScroll(scrollState)
+            .height(200.dp)
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(with(LocalDensity.current) { ((v * 1000) * scale).toDp() })
+        ) {
+            val width = size.width
+            val height = size.height
+            val centerY = height / 2
+            val amplitude = height / 4
+
+            val step = audioData.size / width.toInt()
+            for (i in 0 until width.toInt()) {
+                val sampleIndex = i * step
+                if (sampleIndex < audioData.size) {
+                    val sample = audioData[sampleIndex]
+                    val normalizedSample =
+                        sample.toFloat() / Short.MAX_VALUE.toFloat() * amplitude
+                    drawLine(
+                        color = Color.Blue,
+                        start = androidx.compose.ui.geometry.Offset(
+                            i.toFloat(),
+                            centerY - normalizedSample
+                        ),
+                        end = androidx.compose.ui.geometry.Offset(
+                            i.toFloat(),
+                            centerY + normalizedSample
+                        ),
+                        strokeWidth = 1f
+                    )
+                }
             }
         }
     }
